9894f7647f4b2f5c924ee5dce9d30bf0cb75200d
Tuan Anh <tuananhkbhb@yahoo.com.vn>
2019-11-15 18:27:12 +0700

    MVCCoreApp

---
+++ MVCCoreApp/MVCCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCCoreApp.Models;
+
+namespace MVCCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new IndexModel();
+            model.Message = "Hello my name tuan anh";
+            return View(model);
+        }
+    }
+}
